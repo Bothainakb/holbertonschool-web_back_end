8efd1655121514f6a23b9b2d819e5da293a06785
bothainakb <bothainakb@gmail.com>
2026-09-17 08:54:17 +0000

    10-car.js

---
+++ ES6_classes/10-car.js
@@ -0,0 +1,13 @@
+#!/usr/bin/env node
+
+export default class Car {
+  constructor(brand, motor, color) {
+    this._brand = brand;
+    this._motor = motor;
+    this._color = color;
+  }
+
+  cloneCar() {
+    return new this.constructor();
+  }
+}
